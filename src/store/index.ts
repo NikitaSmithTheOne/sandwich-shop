9361ccf632e5c6e29693bafa6f3c4f91368da74c
Nikita Galkin <push.default>
2021-04-21 16:36:45 +0500

    refactor: 💡 redux store: save exact param to ls + throttle

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,6 @@
 // *** NPM ***
 import { createStore, compose, combineReducers } from 'redux';
+import { throttle } from 'lodash';
 
 // *** OTHER ***
 import ingredientsReducer from './ingredients';
@@ -7,6 +8,7 @@ import { loadFromLocalStorage, saveToLocalStorage } from '../utils/local-storage
 
 // *** CONSTANTS ***
 const LOCAL_STORAGE_REDUX_NAME = 'REDUX_STATE';
+const LOCAL_STORAGE_THROTTLE_TIME = 1000;
 
 // *** REDUX STORE ***
 const composeEnhancers =
@@ -25,9 +27,11 @@ const store = createStore(
     composeEnhancers(),
 );
 
-store.subscribe(() => {
-    saveToLocalStorage(store.getState(), LOCAL_STORAGE_REDUX_NAME);
-});
+store.subscribe(
+    throttle(() => {
+        saveToLocalStorage({ ingredients: store.getState().ingredients }, LOCAL_STORAGE_REDUX_NAME);
+    }, LOCAL_STORAGE_THROTTLE_TIME),
+);
 
 export type StoreType = ReturnType<typeof rootReducer>;
 export type StoreDispatchType = typeof store.dispatch;
